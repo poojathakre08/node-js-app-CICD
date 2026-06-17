755dd283cfd7a336a84452c8c75c832b394d36d5
poojathakre08 <poojathakre2006@gmail.com>
2026-06-17 08:23:11 +0530

    modified app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('hiiiii from jenkins, added webhook, we are from 18 May Devops batch');
+  res.send('hello from jenkins, added webhook, we are from 18 May Devops batch');
 });
 
 app.listen(port, () => {
