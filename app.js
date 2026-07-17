67f548c8b867e1e00c74f0060a90378ef8c323e2
poojathakre08 <poojathakre2006@gmail.com>
2026-07-17 06:42:49 +0530

    modified app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('hello from jenkins, added webhook, we are from 18 May Devops batch');
+  res.send('hello from jenkins, added webhook so that github directly push the code on target server, this in node pipeline cicd executed on target server');
 });
 
 app.listen(port, () => {
